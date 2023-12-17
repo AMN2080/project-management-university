d37d4e9c473a44867fbebe8765cd4c9b1b9aacaa
Alireza Mohamadzade <alireza3205@gmail.com>
2023-12-17 20:43:44 +0330

    error fix and add tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { MuiThemeProvider, createTheme } from '@mui/material/styles';
 
 // تنظیم فونت
-const defaultFont = createMuiTheme({
+const defaultFont = createTheme({
   typography: {
     "fontFamily": `"Vazirmatn", "Roboto", sans-serif`,
     "fontSize": 14,
