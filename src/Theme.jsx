f00597380c1c4f6b01e5fd76fb61b855d76fef49
Alireza Mohamadzade <alireza3205@gmail.com>
2023-12-18 21:22:20 +0330

    add dark mode

---
+++ src/Theme.jsx
@@ -0,0 +1,36 @@
+import createTheme from '@mui/material/styles/createTheme';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import {useMemo} from "react"
+
+function Theme(){
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  
+  const getDesignTokens = (prefersDarkMode) => ({
+    typography: {
+      "fontFamily": `"Vazirmatn", "Roboto", sans-serif`,
+      "fontSize": 14,
+      "fontWeightLight": 300,
+      "fontWeightRegular": 400,
+      "fontWeightMedium": 500
+    },
+    palette: {
+      mode: prefersDarkMode ? 'dark' : 'light',
+    },
+  });
+  
+  const theme = useMemo(() => createTheme(getDesignTokens(prefersDarkMode)), [prefersDarkMode])
+  
+  return(
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  )
+}
+
+//const theme = createTheme(getDesignTokens('light'))
+
+export default Theme;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import theme from './theme.jsx';
-import CssBaseline from '@mui/material/CssBaseline';
-import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import Theme from './Theme'
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <CssBaseline />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Theme />
+    </BrowserRouter>
   </React.StrictMode>,
 )
