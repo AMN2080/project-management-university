556f69c5772d7796c0c4def47fc15931222eb0d9
Alireza Mohamadzade <alireza3205@gmail.com>
2023-12-19 13:24:45 +0330

    delete trash in offcanvas

--- src/components/offcanvas/Offcanvas.jsx
+++ src/components/offcanvas/Offcanvas.jsx
@@ -1,4 +1,4 @@
-import {Fragment, useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
