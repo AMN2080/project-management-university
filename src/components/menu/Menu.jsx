2ae5c3ea494da8406083784ccbc67a88e61c04f4
Alireza Mohamadzade <alireza3205@gmail.com>
2023-12-20 19:20:37 +0330

    no reloading

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
@@ -19,25 +20,29 @@ function Menu(){
     <Box bgcolor="primary.main" className="hidden text-white lg:block fixed w-80 h-full space-y-4 p-5">
       <List>
         <ListItem disablePadding>
-          <ListItemButton href='/home'>
-            <ListItemIcon className="flex justify-between">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 pl-2 text-indigo-300">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-              </svg>
-            </ListItemIcon>
-            <ListItemText primary="داشبورد" />
-          </ListItemButton>
+          <Link className='w-full' to='/home'>
+            <ListItemButton>
+                <ListItemIcon className="flex justify-between">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 pl-2 text-indigo-300">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                  </svg>
+                </ListItemIcon>
+                <ListItemText primary="داشبورد" />
+            </ListItemButton>
+          </Link>
         </ListItem>
         {menu.map((item) => (
           <ListItem key={item.id} disablePadding>
-            <ListItemButton href={item.route}>
-              <ListItemIcon>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 pl-2 text-indigo-300">
-                  <path strokeLinecap="round" strokeLinejoin="round" d={item.dSVG} />
-                </svg>
-              </ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItemButton>
+            <Link className='w-full' to={item.route}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 pl-2 text-indigo-300">
+                    <path strokeLinecap="round" strokeLinejoin="round" d={item.dSVG} />
+                  </svg>
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </Link>
           </ListItem>
         ))}
         <ListItem disablePadding>
