7a31e7e165a56c4aedee5b1bcce4ee60f5776ebb
Alireza Mohamadzade <alireza3205@gmail.com>
2023-12-22 18:37:56 +0330

    create ProjectDetails & Projects

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,12 +3,16 @@ import Dashboard from './pages/dashboard/Dashboard'
 import ReportProject from './pages/reportProject/ReportProject'
 import AddProject from './pages/addProject/AddProject'
 import NotFound from './pages/notFound/NotFound'
+import Projects from './pages/projects/Projects'
+import ProjectDetails from './pages/projectDetails/ProjectDetails'
 
 let Routes = [
   {path:'/' , element:<Login />},
   {path:'/home' , element:<Dashboard />},
   {path:'/report-project' , element:<ReportProject />},
   {path:'/create-project' , element:<AddProject />},
+  {path:'/projects' , element:<Projects />},
+  {path:'/project-details' , element:<ProjectDetails />},
   {path:'/*' , element:<NotFound />},
 ]
 
